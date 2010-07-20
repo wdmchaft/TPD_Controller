4222edfc825ab173e959f35415d0202724c4e077
Christopher Heidelberger <heidelbergerc@gmail.com>
2010-07-20 17:06:22 -0400

    Adjusted simulation.
    Adjusted resistance function and transfer function for simulation.  Saved as simulation 4.  Does not work as well as original simulation.

--- TPD_data_load.m
+++ TPD_data_load.m
@@ -39,17 +39,17 @@ power = voltage.*current;
 
 %plot data
 plot(time, temp, 'r-', time, tempSet, 'g-','linewidth', 1.5);
-axis ([0 60 0 550]);
+axis ([0 60 0 750]);
 grid on;
-h_title = title('Kp = 0.002');
+h_title = title('Kp = 0.015');
 h_xlabel = xlabel('Time [s]');
 h_ylabel = ylabel('Temperature [C]');
 h_legend = legend('Measured Temperature (Thermocouple)', 'Temperature Setpoint', 'location',...
     'southeast');
-set(h_title, 'FontSize', 14);
-set(h_xlabel, 'FontSize', 12);
-set(h_ylabel, 'FontSize', 12);
-set(h_legend, 'FontSize', 12);
+set(h_title, 'FontSize', 18);
+set(h_xlabel, 'FontSize', 15);
+set(h_ylabel, 'FontSize', 15);
+set(h_legend, 'FontSize', 15);
 whitebg('white');
 
 fclose(fid);
--- simulation_graph.m
+++ simulation_graph.m
@@ -3,17 +3,19 @@
 
 simtemp.time = simtemp.time - simtemp.time(41);
 simsetpoint.time = simsetpoint.time - simsetpoint.time(41);
-
+hold off;
 plot(simsetpoint.time(41:end), simsetpoint.signals.values(41:end),...
-    'g-', 'LineWidth', 1.5)
-hold all;
-plot(simtemp.time(41:end),simtemp.signals.values(41:end),...
-    'r-','LineWidth', 1.5)
-
-
-title('Kp = 0.003 (Simulated)');
-xlabel('Time [s]');
-ylabel('Temperature [C]');
-legend('Temperature', 'Temperature Setpoint', 'location',...
+    'g-', simtemp.time(41:end),simtemp.signals.values(41:end),...
+    'r-', 'LineWidth', 1.5)
+axis ([0 60 0 750]);
+grid on;
+h_title = title('Kp = 0.015 (Simulated)');
+h_xlabel = xlabel('Time [s]');
+h_ylabel = ylabel('Temperature [C]');
+h_legend = legend('Sample Temperature', 'Temperature Setpoint', 'location',...
     'southeast');
+set(h_title, 'FontSize', 18);
+set(h_xlabel, 'FontSize', 15);
+set(h_ylabel, 'FontSize', 15);
+set(h_legend, 'FontSize', 15);
 whitebg('white');

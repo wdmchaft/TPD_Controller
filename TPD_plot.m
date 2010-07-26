04c8c52a92e47224c3ec598f4f0594e2147789ed
Christopher Heidelberger <heidelbergerc@gmail.com>
2010-07-26 18:02:46 -0400

    Added m file to graph TPD data.

---
+++ TPD_plot.m
@@ -0,0 +1,16 @@
+%TPD Plot
+%Christopher Heidelberger
+%Plots TPD data.  User must enter variable names manually before running
+%script.
+
+plot(rgaTemp,H2Shift,'linewidth', 1.5);
+grid on;
+h_title = title('TPD: H on Si');
+h_xlabel = xlabel('Temperature [C]');
+h_ylabel = ylabel('Partial Pressure [torr]');
+h_legend = legend('Hydrogen (H2)','northeast');
+set(h_title, 'FontSize', 18);
+set(h_xlabel, 'FontSize', 15);
+set(h_ylabel, 'FontSize', 15);
+set(h_legend, 'FontSize', 15);
+whitebg('white');
